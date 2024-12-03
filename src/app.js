96b5919aa148e4ec6772803734195375192f02e3
Mitanshu Agrawal <mishu10005agrawal@gmail.com>
2024-12-03 21:53:29 +0530

    chore: Comment out CORS configuration in app.js

--- src/app.js
+++ src/app.js
@@ -5,8 +5,8 @@ import express  from "express";
 const app=express();
 
 app.use(cors({
-origin:process.env.CORS_ORIGIN,
-Credentials:true
+// origin:process.env.CORS_ORIGIN,
+// Credentials:true
 }))
 app.use(express.json({limit:'16kb'}));
 app.use(express.urlencoded({extended:true,limit:'16kb'}));
