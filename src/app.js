9af0c82c561afc68e1af660468236ab4fcf5e083
Mitanshu Agrawal <mishu10005agrawal@gmail.com>
2024-12-03 21:44:41 +0530

    npm start added

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import express  from "express";
 const app=express();
 
 app.use(cors({
-origin:process.nextTick.CORS_ORIGIN,
+origin:process.env.CORS_ORIGIN,
 Credentials:true
 }))
 app.use(express.json({limit:'16kb'}));
